d92c5deb2acc8937a70782d37d424f57bb18549f
Luiz Felipe Menezes Moreira <2403662@SALAS.AULAS>
2026-04-07 11:28:54 -0300

    Adicionar rota multiplicar com teste falhando

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ app = FastAPI()
 def read_root():
     return {"mensagem": "API funcionando!"}
 
-@app.get("/somar/{a}/{b}")
-def somar(a: int, b: int):
-    return {"resultado": a + b}  # Corrigido
+@app.get("/multiplicar/{a}/{b}")
+def multiplicar(a: int, b: int):
+    return {"resultado": a * b}
